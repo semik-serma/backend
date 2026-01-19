f3f2a8a955bc8733336dabae0df91bf17ee1eb31
semik-serma <semikserma@gmail.com>
2026-01-19 22:40:38 +0545

    hosting

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { connectdb } from './src/config/db.js';
 import cors from 'cors'
 import route from './src/router/auth.route.js';
-// import { articleRoute } from './src/router/article.route.js';
 import { commentroute } from './src/router/comment.route.js';
 import { countrydetect } from './src/router/countrydetect.route.js';
 import Articleroute from './src/router/article.route.js';
@@ -12,14 +11,14 @@ import { visitorroute } from './src/router/visitor.route.js';
 
 const app=express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:['http://localhost:3000','https://frontend-mu.vercel.app/'],credentials:true}))
 connectdb()
 app.use('/auth',route)
 app.use('/article',Articleroute)
 app.use('/',commentroute)
 app.use('/countrydetect',countrydetect)
-app.use('/',contactroute)
-app.use('/',visitorroute)
+app.use('/contact',contactroute)
+app.use('/visit',visitorroute)
 
 
 
